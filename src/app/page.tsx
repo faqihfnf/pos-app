5f3c0cea384436afaeacfdaef57cabb98abf91f9
faqihfnf <faqih.fnf@gmail.com>
2025-10-12 15:30:49 +0700

    Add state management fo profile user with zustand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useAuthStore } from "@/stores/auth-store";
 import Link from "next/link";
 
 export default function Home() {
+  const profile = useAuthStore((state) => state.profile);
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-3">
-      <h1 className="text-2xl font-bold">Welcome User</h1>
+    <div className="flex h-screen flex-col items-center justify-center space-y-4">
+      <h1 className="text-2xl font-bold">Welcome back, {profile.name} </h1>
       <Link href="/admin">
         <Button variant="primary">Dashboard</Button>
       </Link>
