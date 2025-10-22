39ed2b5aba386413f49ecef096f1b18650e75076
faqihfnf <faqih.fnf@gmail.com>
2025-10-22 23:23:06 +0700

    add pagination on user page dashboard

--- src/app/(dashboard)/admin/user/_components/user.tsx
+++ src/app/(dashboard)/admin/user/_components/user.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { HEADER_TABLE_USER } from "@/constants/user-constant";
+import useDataTable from "@/hooks/use-data-table";
 import { createClient } from "@/lib/supabase/client";
 import { useQuery } from "@tanstack/react-query";
 import { Pencil, Trash2 } from "lucide-react";
@@ -14,24 +15,28 @@ import { toast } from "sonner";
 
 export default function UserManagement() {
   const supabase = createClient();
+  const { currentPage, handleChangePage, currentLimit, handleChangeLimit } =
+    useDataTable();
   const { data: users, isLoading } = useQuery({
-    queryKey: ["users"],
+    queryKey: ["users", currentPage, currentLimit],
     queryFn: async () => {
-      const { data, error } = await supabase
+      const result = await supabase
         .from("profiles")
         .select("*", { count: "exact" })
+        .range((currentPage - 1) * currentLimit, currentPage * currentLimit - 1)
         .order("created_at");
 
-      if (error) {
-        toast.error("Failed to fetch users", { description: error.message });
-        return [];
+      if (result.error) {
+        toast.error("Failed to fetch users", {
+          description: result.error.message,
+        });
       }
-      return data;
+      return result;
     },
   });
 
   const filteredData = useMemo(() => {
-    return (users || []).map((user, index) => {
+    return (users?.data || []).map((user, index) => {
       return [
         index + 1,
         user.id,
@@ -68,6 +73,12 @@ export default function UserManagement() {
     });
   }, [users]);
 
+  const totalPages = useMemo(() => {
+    return users && users.count !== null
+      ? Math.ceil(users.count / currentLimit)
+      : 0;
+  }, [users]);
+
   return (
     <div className="w-full">
       <div className="mb-4 flex w-full flex-col justify-between gap-2 lg:flex-row">
@@ -85,6 +96,11 @@ export default function UserManagement() {
         header={HEADER_TABLE_USER}
         isLoading={isLoading}
         data={filteredData}
+        totalPages={totalPages}
+        currentPage={currentPage}
+        currentLimit={currentLimit}
+        onChangePage={handleChangePage}
+        onChangeLimit={handleChangeLimit}
       />
     </div>
   );
--- src/constants/auth-constant.ts
+++ src/constants/auth-constant.ts
@@ -1,5 +1,3 @@
-import { email } from "zod";
-
 export const INITIAL_LOGIN_FORM = {
   email: "",
   password: "",
